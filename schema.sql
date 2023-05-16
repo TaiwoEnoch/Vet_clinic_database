fa6d54da58524609e2ba6ab5b46e8a991fddad43
Enoch Taiwo <111912283+TaiwoEnoch@users.noreply.github.com>
2023-05-16 15:55:08 +0100

    Update schema.sql

--- schema.sql
+++ schema.sql
@@ -1,10 +1,2 @@
 /* Database schema to keep the structure of entire database. */
 
-Create Table animals (
-Id Integer Primary Key,
-	name varchar(100),
-	date_of_birth date,
-	escape_attempt Integer,
-	neutered boolean,
-	weight_kg decimal(5, 2)
-);
