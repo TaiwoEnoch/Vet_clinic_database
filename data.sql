c71fdd04ef2f2885e2902c3f85f6323a8e149fad
Enoch Taiwo <111912283+TaiwoEnoch@users.noreply.github.com>
2023-05-19 07:40:58 +0100

    Update the data of the "animals" table in data.sql:

--- data.sql
+++ data.sql
@@ -1,13 +1,25 @@
 /* Populate database with sample data. */
 
-INSERT INTO animals (
-	name,
-	date_of_birth,
-	escape_attempts,
-	neutered,
-	weight_kg
-) VALUES
-('Agumon', '2020-02-03', 0, true, 10.23),
-('Gabumon', '2018-11-15', 2, true, 8),
-('Pikachu', '2021-01-07', 1, false, 15.04),
-('Devimon', '2017-05-12', 5, true, 11);
+CREATE TABLE animals (
+ id SERIAL PRIMARY KEY,
+  name VARCHAR(300),
+  birthdate DATE,
+  escape_attempts INTEGER,
+  neutered BOOLEAN,
+  weight DECIMAL(5, 2)
+  
+);
+
+ALTER TABLE animals
+ADD COLUMN species VARCHAR(255);
+
+INSERT INTO animals (name, birthdate, weight, neutered, escape_attempts, species)
+VALUES ('Charmander', '2020-02-08', -11, false, 0, NULL),
+       ('Plantmon', '2021-11-15', -5.7, true, 2, NULL),
+       ('Squirtle', '1993-04-02', -12.13, false, 3, NULL),
+       ('Angemon', '2005-06-12', -45, true, 1, NULL),
+       ('Boarmon', '2005-06-07', 20.4, true, 7, NULL),
+       ('Blossom', '1998-10-13', 17, true, 3, NULL),
+       ('Ditto', '2022-05-14', 22, true, 4, NULL);
+
+SELECT * FROM animals
