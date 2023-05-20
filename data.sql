ec7f57bee13a273a046bb9e518b0580a497e4441
Enoch Taiwo <111912283+TaiwoEnoch@users.noreply.github.com>
2023-05-20 14:14:31 +0100

    Update data.sql by adding data to the owners table

--- data.sql
+++ data.sql
@@ -11,3 +11,11 @@ INSERT INTO animals (
 ('Gabumon', '2018-11-15', 2, true, 8),
 ('Pikachu', '2021-01-07', 1, false, 15.04),
 ('Devimon', '2017-05-12', 5, true, 11);
+
+INSERT INTO owners (full_name, age) VALUES
+  ('Sam Smith', 34),
+  ('Jennifer Orwell', 19),
+  ('Bob', 45),
+  ('Melody Pond', 77),
+  ('Dean Winchester', 14),
+  ('Jodie Whittaker', 38);
