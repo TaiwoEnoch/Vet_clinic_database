10e6d9d3d56e5c89266b1a8b792ce8d0e7ee9e1f
Enoch Taiwo <111912283+TaiwoEnoch@users.noreply.github.com>
2023-05-20 11:25:01 +0100

    Insert the provided data into the 'animals' table for updating it

--- data.sql
+++ data.sql
@@ -3,23 +3,20 @@
 CREATE TABLE animals (
  id SERIAL PRIMARY KEY,
   name VARCHAR(300),
-  birthdate DATE,
+  date_of_birth DATE,
   escape_attempts INTEGER,
   neutered BOOLEAN,
-  weight DECIMAL(5, 2)
+  weight_kg DECIMAL(5, 2)
   
 );
 
-ALTER TABLE animals
-ADD COLUMN species VARCHAR(255);
-
-INSERT INTO animals (name, birthdate, weight, neutered, escape_attempts, species)
-VALUES ('Charmander', '2020-02-08', -11, false, 0, NULL),
-       ('Plantmon', '2021-11-15', -5.7, true, 2, NULL),
-       ('Squirtle', '1993-04-02', -12.13, false, 3, NULL),
-       ('Angemon', '2005-06-12', -45, true, 1, NULL),
-       ('Boarmon', '2005-06-07', 20.4, true, 7, NULL),
-       ('Blossom', '1998-10-13', 17, true, 3, NULL),
-       ('Ditto', '2022-05-14', 22, true, 4, NULL);
+INSERT INTO animals (name, birthdate, weight, neutered, escape_attempts)
+VALUES ('Charmander', '2020-02-08', -11, false, 0),
+       ('Plantmon', '2021-11-15', -5.7, true, 2),
+       ('Squirtle', '1993-04-02', -12.13, false, 3),
+       ('Angemon', '2005-06-12', -45, true, 1),
+       ('Boarmon', '2005-06-07', 20.4, true, 7),
+       ('Blossom', '1998-10-13', 17, true, 3),
+       ('Ditto', '2022-05-14', 22, true, 4);
 
 SELECT * FROM animals
