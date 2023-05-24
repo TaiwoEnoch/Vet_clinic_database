dc7ae68ca6c8375afed74b41913cb73143d60860
TaiwoEnoch <taiwoenoch0@gmail.com>
2023-05-24 08:56:02 +0100

    Update data and schemal file

--- data.sql
+++ data.sql
@@ -1,99 +1,12 @@
-/* Populate database with sample data. */
+INSERT INTO animals (name) VALUES ('Agumon'), ('Gabumon'), ('Pikachu'), ('Devimon'), ('Charmander'), ('Plantmon'), ('Squirtle'), ('Angemon'), ('Boarmon'), ('Blossom');
+
+INSERT INTO vets (name) VALUES ('William Tatcher'), ('Maisy Smith'), ('Stephanie Mendez'), ('Jack Harkness');
+  
 
 
-INSERT INTO animals (
-	name,
-	date_of_birth,
-	escape_attempts,
-	neutered,
-	weight_kg
-) VALUES
-('Agumon', '2020-02-03', 0, true, 10.23),
-('Gabumon', '2018-11-15', 2, true, 8),
-('Pikachu', '2021-01-07', 1, false, 15.04),
-('Devimon', '2017-05-12', 5, true, 11);
 
-INSERT INTO owners (full_name, age) VALUES
-  ('Sam Smith', 34),
-  ('Jennifer Orwell', 19),
-  ('Bob', 45),
-  ('Melody Pond', 77),
-  ('Dean Winchester', 14),
-  ('Jodie Whittaker', 38);
-  
-  INSERT INTO species (name) VALUES
-  ('Pokemon'),
-  ('Digimon');
-  
-UPDATE animals
-SET species_id = CASE
-  WHEN name LIKE '%mon' THEN (SELECT id FROM species WHERE name = 'Digimon')
-  ELSE (SELECT id FROM species WHERE name = 'Pokemon')
-  END;
+-- This will add 3.594.280 visits considering you have 10 animals, 4 vets, and it will use around ~87.000 timestamps (~4min approx.)
+INSERT INTO visits (animal_id, vet_id, date_of_visit) SELECT * FROM (SELECT id FROM animals) animal_ids, (SELECT id FROM vets) vets_ids, generate_series('1980-01-01'::timestamp, '2021-01-01', '4 hours') visit_timestamp;
 
-UPDATE animals AS a
-SET owner_id = o.id
-FROM owners AS o
-WHERE a.owner_id IS NULL
-  AND (
-    (a.name = 'Agumon' AND o.full_name = 'Sam Smith') OR
-    (a.name IN ('Gabumon', 'Pikachu') AND o.full_name = 'Jennifer Orwell') OR
-    (a.name IN ('Devimon', 'Plantmon') AND o.full_name = 'Bob') OR
-    (a.name IN ('Charmander', 'Squirtle', 'Blossom') AND o.full_name = 'Melody Pond') OR
-    (a.name IN ('Angemon', 'Boarmon') AND o.full_name = 'Dean Winchester')
-  );
-=======
-CREATE TABLE animals (
- id SERIAL PRIMARY KEY,
-  name VARCHAR(300),
-  date_of_birth DATE,
-  escape_attempts INTEGER,
-  neutered BOOLEAN,
-  weight_kg DECIMAL(5, 2)
-  
-);
-
-INSERT INTO animals (name, birthdate, weight, neutered, escape_attempts)
-VALUES ('Charmander', '2020-02-08', -11, false, 0),
-       ('Plantmon', '2021-11-15', -5.7, true, 2),
-       ('Squirtle', '1993-04-02', -12.13, false, 3),
-       ('Angemon', '2005-06-12', -45, true, 1),
-       ('Boarmon', '2005-06-07', 20.4, true, 7),
-       ('Blossom', '1998-10-13', 17, true, 3),
-       ('Ditto', '2022-05-14', 22, true, 4);
-
-INSERT INTO vets (name, age, date_of_graduation) VALUES
-  ('William Tatcher', 45, '2000-04-23'),
-  ('Maisy Smith', 26, '2019-01-17'),
-  ('Stephanie Mendez', 64, '1981-05-04'),
-  ('Jack Harkness', 38, '2008-06-08');
-  
-  INSERT INTO specializations (vet_id, species) VALUES
-  (1, 'Pokemon'),
-  (3, 'Digimon'),
-  (3, 'Pokemon'),
-  (4, 'Digimon');
-  
-  
-  INSERT INTO visits (animal, vet_id, visit_date) VALUES
-  ('Agumon', 1, '2020-05-24'),
-  ('Agumon', 3, '2020-07-22'),
-  ('Gabumon', 4, '2021-02-02'),
-  ('Pikachu', 2, '2020-01-05'),
-  ('Pikachu', 2, '2020-03-08'),
-  ('Pikachu', 2, '2020-05-14'),
-  ('Devimon', 3, '2021-05-04'),
-  ('Charmander', 4, '2021-02-24'),
-  ('Plantmon', 1, '2020-08-10'),
-  ('Plantmon', 2, '2019-12-21'),
-  ('Plantmon', 2, '2021-04-07'),
-  ('Squirtle', 3, '2019-09-29'),
-  ('Angemon', 4, '2020-10-03'),
-  ('Angemon', 4, '2020-11-04'),
-  ('Boarmon', 2, '2019-01-24'),
-  ('Boarmon', 2, '2019-05-15'),
-  ('Boarmon', 2, '2020-02-27'),
-  ('Boarmon', 2, '2020-08-03'),
-  ('Blossom', 3, '2020-05-24'),
-  ('Blossom', 1, '2021-01-11');
-  
+-- This will add 2.500.000 owners with full_name = 'Owner <X>' and email = 'owner_<X>@email.com' (~2min approx.)
+insert into owners (full_name, email) select 'Owner ' || generate_series(1,2500000), 'owner_' || generate_series(1,2500000) || '@mail.com';
